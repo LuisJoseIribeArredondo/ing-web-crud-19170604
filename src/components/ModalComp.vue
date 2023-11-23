b8ab9279912f292f7f862cc7847e1cac0f0406a1
Luis Jose Iribe Arredondo <19170604@culiacan.tecnm.mx>
2023-11-22 17:42:16 -0700

    Agregar empleados y mostararlos

---
+++ src/components/ModalComp.vue
@@ -0,0 +1,180 @@
+<template>
+  <div>
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
+      Agregar Empleado
+    </button>
+
+    <div class="modal" tabindex="-1" role="dialog" v-show="mostrar"  id="myModal">
+      <div class="modal-dialog modal-dialog-scrollable">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title">Agregar Empleados</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                
+            
+          </div>
+          <div class="modal-body">
+            <div class="">
+              <!--Id-->
+              <label for="lblIdEmp" class="form-label">Id del Empleado</label>
+              <input
+                type="text"
+                class="form-control inputEmpleado"
+                id="inputId"
+                placeholder="Id del empleado"
+                v-model="miEmpleado.id"
+              />
+              <hr class="division" />
+              <!--Nombre-->
+              <label for="lblNombreEmp" class="form-label"
+                >Nombre del Empleado</label
+              >
+              <input
+                type="text"
+                class="form-control inputEmpleado"
+                id="inputNombre"
+                placeholder="nombre del empleado"
+                v-model="miEmpleado.nombre"
+              />
+              <hr class="division" />
+              <!--Departamento-->
+              <label for="lblDepartamentoEmp" class="form-label"
+                >Departamento del Empleado</label
+              >
+              <input
+                type="text"
+                class="form-control inputEmpleado"
+                id="inputDepartamento"
+                placeholder="Departamento del empleado"
+                v-model="miEmpleado.departamento"
+              />
+              <hr class="division" />
+              <!--Salario-->
+              <label for="lblSalarioEmp" class="form-label"
+                >Salario del Empleado</label
+              >
+              <input
+                type="text"
+                class="form-control inputEmpleado"
+                id="inputSalario"
+                placeholder="Salario del empleado"
+                v-model="miEmpleado.salario"
+              />
+              <hr class="division" />
+              <!--Fecha de contratacion-->
+              <label for="lblNombreEmp" class="form-label"
+                >Fecha de contratacion</label
+              >
+              <input type="date" class="form-control inputEmpleado" id="inputFecha" v-model="miEmpleado.fecha"/>
+              <hr class="division" />
+              <!--Creado en-->
+              <label for="lblNombreEmp" class="form-label">Creado en</label>
+              <input type="date" class="form-control inputEmpleado" id="inputCreado" v-model="miEmpleado.creado"/>
+              <hr class="division" />
+            </div>
+        </div>
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" @click="agregarEmpleado"> Guardar</button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <div class="card-body col d-flex justify-content-center" v-for="miEmpleado in empleados">
+    <div class="accordion" id="accordionExample">
+  <div class="accordion-item" >
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        -Empleado: {{ miEmpleado.nombre }}
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body text-start">
+        <p>ID: {{ miEmpleado.id }}</p> <br>
+        <p>Nombre: {{ miEmpleado.nombre }}</p> <br>
+        <p>Departamento: {{ miEmpleado.departamento }}</p> <br>
+        <p>Salario del empleado: {{ miEmpleado.salario }}</p> <br>
+        <p>Fecha de contratacion: {{ miEmpleado.fecha }}</p> <br>
+        <p>Fecha de creacion: {{ miEmpleado.creado }}</p> <br>
+      </div>
+    </div>
+  </div>
+  </div>
+
+        
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        mostrar: false,
+      empleados: [],
+      miEmpleado: {
+        id: "",
+        nombre: "",
+        departamento: "",
+        salario: "",
+        fecha: "",
+        creado: "",
+      },
+    };
+  },
+  mounted(){
+    this.recuperarEmpleados();
+  },
+  methods: {
+    
+    mostrarModal(){
+      this.mostrar = true;
+    },
+    cerrarModal(){
+      myModal.toggle()
+    },
+    agregarEmpleado() {
+      if (
+        this.miEmpleado.id &&
+        this.miEmpleado.nombre &&
+        this.miEmpleado.departamento &&
+        this.miEmpleado.salario &&
+        this.miEmpleado.fecha &&
+        this.miEmpleado.creado
+      ) {
+        // Agregar el objeto al arreglo empleados
+        this.empleados.push({ ...this.miEmpleado });
+
+        // Guardar empleados en localStorage
+        localStorage.setItem('empleadosTXT', JSON.stringify(this.empleados));
+
+        // Limpiar el objeto miEmpleado después de agregarlo
+        this.miEmpleado.id = "";
+        this.miEmpleado.nombre = "";
+        this.miEmpleado.departamento = "";
+        this.miEmpleado.salario = "";
+        this.miEmpleado.fecha = "";
+        this.miEmpleado.creado = "";
+        alert("Empleado agregado con exito!!!");
+        this.mostrar= false;
+      } else {
+        alert(
+          "Por favor, complete todos los campos antes de agregar un empleado."
+        );
+      }
+    },
+    recuperarEmpleados() {
+      // Recuperar empleados del localStorage
+      const storedEmpleados = localStorage.getItem('empleadosTXT');
+      if (storedEmpleados) {
+        // Convertir la cadena JSON a un objeto JavaScript y asignar a empleados
+        this.empleados = JSON.parse(storedEmpleados);
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,74 +2,14 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Mis Empleados VueJs" />
-    <div class="mb-3">
-      <!--Id-->
-      <label for="lblIdEmp" class="form-label">Id del Empleado</label>
-      <input
-        type="text"
-        class="form-control inputEmpleado"
-        id="inputId"
-        placeholder="Id del empleado"
-        v-model="miEmpleado.id"
-      />
-      <hr class="division" />
-      <!--Nombre-->
-      <label for="lblNombreEmp" class="form-label">Nombre del Empleado</label>
-      <input
-        type="text"
-        class="form-control inputEmpleado"
-        id="inputNombre"
-        placeholder="nombre del empleado"
-        v-model="miEmpleado.nombre"
-      />
-      <hr class="division" />
-      <!--Departamento-->
-      <label for="lblDepartamentoEmp" class="form-label"
-        >Departamento del Empleado</label
-      >
-      <input
-        type="text"
-        class="form-control inputEmpleado"
-        id="inputDepartamento"
-        placeholder="Departamento del empleado"
-        v-model="miEmpleado.departamento"
-      />
-      <hr class="division" />
-      <!--Salario-->
-      <label for="lblSalarioEmp" class="form-label">Salario del Empleado</label>
-      <input
-        type="text"
-        class="form-control inputEmpleado"
-        id="inputSalario"
-        placeholder="Salario del empleado"
-        v-model="miEmpleado.salario"
-      />
-      <hr class="division" />
-      <!--Fecha de contratacion-->
-      <label for="lblNombreEmp" class="form-label">Fecha de contratacion</label>
-      <input
-        type="date"
-        class="form-control inputEmpleado"
-        id="inputFecha"
-        v-model="miEmpleado.fecha"
-      />
-      <hr class="division" />
-      <!--Creado en-->
-      <label for="lblNombreEmp" class="form-label">Creado en</label>
-      <input
-        type="date"
-        class="form-control inputEmpleado"
-        id="inputCreado"
-        v-model="miEmpleado.creado"
-      />
-      <hr class="division" />
-    </div>
+    
     <div>
-      <button type="button" class="btn btn-primary" @click="agregarEmpleado">
-        Agregar empleado
-      </button>
+      
+
+      <ModalComp/>
     </div>
-    <h5>{{ miEmpleado }}</h5>
+    
+    <!--<h5>{{ miEmpleado }}</h5>-->
     <!--<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
       <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
         <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
@@ -83,15 +23,18 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import ModalComp from "@/components/ModalComp.vue";
 
 export default {
   name: "HomeView",
   components: {
+    ModalComp,
     HelloWorld,
+
   },
   data() {
     return {
-      empleados: [],
+      /*empleados: [],
       miEmpleado: {
         id: "",
         nombre: "",
@@ -99,10 +42,13 @@ export default {
         salario: "",
         fecha: "",
         creado: "",
-      },
+      },*/
+      mostrarModal: false,
     };
   },
   methods: {
+
+
     agregarEmpleado() {
       if (
         this.miEmpleado.id &&
@@ -130,6 +76,14 @@ export default {
         alert(
           "Por favor, complete todos los campos antes de agregar un empleado."
         );
+      }
+    },
+    recuperarEmpleados() {
+      // Recuperar empleados del localStorage
+      const storedEmpleados = localStorage.getItem('empleados');
+      if (storedEmpleados) {
+        // Convertir la cadena JSON a un objeto JavaScript y asignar a empleados
+        this.empleados = JSON.parse(storedEmpleados);
       }
     },
   },
